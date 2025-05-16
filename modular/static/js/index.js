77327655469e552ccbe47a74f3331a3d8375eb26
KimByounghan <30745253+KimByounghan@users.noreply.github.com>
2025-05-17 06:27:11 +0900

    flask 백엔드와 프런트엔드 분리

---
+++ modular/static/js/index.js
@@ -0,0 +1,9 @@
+function getMessage() {
+	let message = fetch('/api/message/get').then(response => {
+		return response.text();
+	}).catch(error => {
+		console.log('[Error]fetch.api.message.get:', error);
+	});
+
+	return message;
+}
